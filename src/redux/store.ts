e61550e5d058efecd196ec969d648719b3f7bf29
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-02-19 23:05:51 +0800

    CREATED: RootState interface & configureStore reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
+import expandReducer from "./features/expand/expandSlice";
+import activeLinkReducer from "./features/activeLink/activeLinkSlice";
+
+// Define the shape of your Redux State
+export interface RootState {
+    expander : {
+        expand: boolean;
+    },
+    activatorLink : {
+        activeLink: string;
+    }
+
+}
 
 export default configureStore({
-    reducer: {}
+    reducer: {
+        expander: expandReducer,
+        activatorLink: activeLinkReducer,
+    }
 })
