69588def88058396838335bba58ffaffb98627a5
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-02-19 23:01:40 +0800

    IMPLEMENTED: Routing for files imported from components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import { Button } from "./components"
+import { Route, Routes } from "react-router-dom"
 
-function App() {
+import { 
+  Dashboard,
+  Financial,
+  Footer,
+  Home,
+  Inventory,
+  Menu,
+  SideBar, 
+} from "./components"
+
+const App = () => {
 
   return (
-    <>
-      <Button variant="secondary">Click Me!</Button>
-    </>
+    <div className="bg-orange-500 h-screen flex flex-col justify-between">
+      <div className="flex flex-row">
+        <SideBar />
+        <div className="container bg-sky-500">
+          <Routes>
+            <Route path="/" element={<Home />} index />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/financial" element={<Financial />} />
+            <Route path="/inventory" element={<Inventory />} />
+          </Routes>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
   )
 }
 
