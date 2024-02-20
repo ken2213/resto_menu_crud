3f6f98c652865296d7cc2c3b939164321f20b551
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-02-20 14:17:58 +0800

    ADDED: more components for unified export on components folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,21 @@
 import Dashboard from "./Dashboard";
+import Employees from "./Employees";
 import Financial from "./Financial";
 import Footer from "./Footer";
 import Home from "./Home";
 import Inventory from "./Inventory";
 import Menu from "./Menu";
+import Orders from "./Orders";
 import SideBar from "./SideBar";
 
 export {
     Dashboard,
+    Employees,
     Financial,
     Footer,
     Home,
     Inventory,
     Menu,
+    Orders,
     SideBar,
 }
