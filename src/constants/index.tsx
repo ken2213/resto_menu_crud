637601f54a941eed7067ce57e309e08311f67c3b
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-02-20 16:42:13 +0800

    ADDED: icon size to 20

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -12,36 +12,36 @@ export const sideBarNavLinks: SideBarNavItem[] = [
         id: 1,
         title: "Home",
         directory: "/",
-        icon: <Home />
+        icon: <Home size={20} />
     },
     {
         id: 2,
         title: "Dashboard",
         directory: "/dashboard",
-        icon: <LayoutDashboard />
+        icon: <LayoutDashboard size={20} />
     },
     {
         id: 3,
         title: "Menus",
         directory: "/menu",
-        icon: <UtensilsCrossed />
+        icon: <UtensilsCrossed size={20} />
     },
     {
         id: 4,
         title: "Employees",
         directory: "/employees",
-        icon: <Users />
+        icon: <Users size={20} />
     },
     {
         id: 5,
         title: "Inventory",
         directory: "/inventory",
-        icon: <Boxes />
+        icon: <Boxes size={20} />
     },
     {
         id: 6,
         title: "Orders",
         directory: "/orders",
-        icon: <Truck />
+        icon: <Truck size={20} />
     },
 ]
