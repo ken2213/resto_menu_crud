213e14a42ff0316caf482c1999a61c4ed1bb1445
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-02-20 14:15:23 +0800

    ADDED: more components

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,31 @@ import { Route, Routes } from "react-router-dom"
 
 import { 
   Dashboard,
+  Employees,
   Financial,
   Footer,
   Home,
   Inventory,
   Menu,
+  Orders,
   SideBar, 
 } from "./components"
 
 const App = () => {
 
   return (
-    <div className="bg-orange-500 h-screen flex flex-col justify-between">
-      <div className="flex flex-row">
+    <div className="h-screen flex flex-col justify-between">
+      <div className="flex flex-row w-full">
         <SideBar />
-        <div className="container bg-sky-500">
+        <div className="container bg-indigo-100 text-indigo-800">
           <Routes>
             <Route path="/" element={<Home />} index />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/menu" element={<Menu />} />
+            <Route path="/employees" element={<Employees />} />
             <Route path="/financial" element={<Financial />} />
             <Route path="/inventory" element={<Inventory />} />
+            <Route path="/orders" element={<Orders />} />
           </Routes>
         </div>
       </div>
