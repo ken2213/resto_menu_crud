5fc807a8aac6a94de8c4e0604024351245e412fb
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-02-22 20:54:17 +0800

    IMPLEMENTED: responsive grid container

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,32 @@
 import MenuItem from "./MenuItem"
+import { FoodItem, FoodItems } from "@/constants"
+import MenuItemDetails from "./MenuItemDetails"
 
 const Menu = () => {
   return (
-    <div>
-      <h1>MENU ITEM</h1>
-    </div>
+    <>
+      <div className="w-full">
+        <h1 className="text-[2rem] font-bold">MENU ITEM</h1>
+      </div>
+
+      <div className="w-full flex justify-center">
+        {/* GRID CONTAINER */}
+        <div 
+          className="
+            overflow-y-auto sm:w-[560px] md:w-[690px] lg:w-[940px] xl:w-[1300px] 2xl:w-[1350px]
+            "
+        >
+          {/* GRID */}
+          <div 
+            className="w-full px-4 py-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center xl:justify-items-start gap-x-4 gap-y-8"
+          >
+            {FoodItems.map((foodItem: FoodItem, index: number) => (
+              <MenuItem key={index} foodItem={foodItem} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
