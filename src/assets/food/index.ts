84af0fa0bbd7750d6dc89e5c7d8abf3820785e2b
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-02-22 14:26:19 +0800

    CREATED: asset/food images unified exporter

---
+++ src/assets/food/index.ts
@@ -0,0 +1,9 @@
+import coralBits from "./coral-bits.jpg"
+import kelpShake from "./kelp-shake.jpg"
+import krabbyPatty from "./krabby-patty.jpg";
+
+export {
+    coralBits,
+    kelpShake,
+    krabbyPatty
+}
