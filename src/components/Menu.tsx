a78a2807e9ec9940c54e4789fab2a99d212b47dd
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-02-23 15:51:24 +0800

    IMPLEMENTED: Sheet component from shadcn/ui on MenuItem and MenuItemDetails

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,6 +2,11 @@ import MenuItem from "./MenuItem"
 import { FoodItem, FoodItems } from "@/constants"
 import MenuItemDetails from "./MenuItemDetails"
 
+import { 
+  Sheet,
+  SheetTrigger,
+} from "./ui/sheet"
+
 const Menu = () => {
   return (
     <>
@@ -20,8 +25,26 @@ const Menu = () => {
           <div 
             className="w-full px-4 py-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center xl:justify-items-start gap-x-4 gap-y-8"
           >
+            {/* 
+              This will map all of items of FoodItems 
+              array variable inside of MenuItem component
+            */}
             {FoodItems.map((foodItem: FoodItem, index: number) => (
-              <MenuItem key={index} foodItem={foodItem} />
+              <Sheet>
+                {/*
+                  This will trigger <MenuItemDetails /> component
+                  to slide in on right part of screen
+                */}
+                <SheetTrigger>
+                  <MenuItem key={index} foodItem={foodItem} />
+                </SheetTrigger>
+
+                {/* 
+                  A component containing all details of a 
+                  clicked item.
+                */}
+                <MenuItemDetails key={index} foodItem={foodItem} />
+              </Sheet>
             ))}
           </div>
         </div>
