7146c743c6e3e312f22092aa429cb6881e9fd47a
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-02-23 16:52:16 +0800

    UPDATED: asset/food images unified exporter

--- src/assets/food/index.ts
+++ src/assets/food/index.ts
@@ -1,9 +1,29 @@
 import coralBits from "./coral-bits.jpg"
 import kelpShake from "./kelp-shake.jpg"
-import krabbyPatty from "./krabby-patty.jpg";
+import krabbyPatty from "./krabby-patty.jpg"
+import barnacleChips from "./barnacle-chips.jpg"
+import cannedBread from "./canned-bread.jpg"
+import exploadingPie from "./exploding-pie.jpg"
+import goofyGooberSundae from "./goofy-goober-sundae.jpg"
+import jellyPatty from "./jelly-patty.jpg"
+import kelpJerky from "./kelp-jerky.jpg"
+import krustyKrabPizza from "./krusty-krab-pizza.jpg"
+import nastyPatty from "./nasty-patty.jpg"
+import prettyPatty from "./pretty-patty.jpg"
+import salad from "./salad.jpg";
 
 export {
     coralBits,
     kelpShake,
-    krabbyPatty
+    krabbyPatty,
+    barnacleChips,
+    cannedBread,
+    exploadingPie,
+    goofyGooberSundae,
+    jellyPatty,
+    kelpJerky,
+    krustyKrabPizza,
+    nastyPatty,
+    prettyPatty,
+    salad
 }
