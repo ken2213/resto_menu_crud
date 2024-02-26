9e414dce981791c7910cc48f873af98fe089e23d
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-02-26 08:27:25 +0800

    ADDED: searchQuery

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import expandReducer from "./features/expand/expandSlice";
 import activeLinkReducer from "./features/activeLink/activeLinkSlice";
+import searchQueryReducer from "./features/search/searchSlice";
 
 // Define the shape of your Redux State
 export interface RootState {
@@ -10,12 +11,15 @@ export interface RootState {
     activatorLink : {
         activeLink: string;
     }
-
+    searcherQuery : {
+        searchQuery: string
+    }
 }
 
 export default configureStore({
     reducer: {
         expander: expandReducer,
         activatorLink: activeLinkReducer,
+        searcherQuery: searchQueryReducer,
     }
 })
