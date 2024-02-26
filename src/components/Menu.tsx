6f1aaf8e8f389646f34e5ea506cf881e543d9633
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-02-26 08:31:38 +0800

    ADDED: Search input |-- |IMPLEMENTED: Automatic Searching functionality

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,8 +6,23 @@ import {
   Sheet,
   SheetTrigger,
 } from "./ui/sheet"
+import { useDispatch, useSelector } from "react-redux"
+import { RootState } from "@/redux/store"
+import { setSearchQuery } from "@/redux/features/search/searchSlice"
 
 const Menu = () => {
+  const searchQuery = useSelector((state: RootState) => state.searcherQuery.searchQuery)
+  const dispatch = useDispatch();
+
+  const handleSearchChange = (event: any) => {
+    dispatch(setSearchQuery(event.target.value))
+  }
+
+  // Filtered food items based on search query
+  const filteredFoodItems = FoodItems.filter(foodItem =>
+    foodItem.name.toLowerCase().includes(searchQuery.toLowerCase()) 
+  );
+
   return (
     <>
       <div className="w-full flex justify-center bg-red-900">
@@ -15,6 +30,17 @@ const Menu = () => {
           <h1 className="text-[2rem] sm:text-[3.5rem] md:text-[4rem] lg:text-[4.5rem] font-bold px-10">MENU MANAGER</h1>
         </div>
 
+      </div>
+
+      {/* Search input */}
+      <div className="w-full flex justify-center mt-4">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className="px-4 py-2 border border-gray-300 rounded-md text-gray-800 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        />
       </div>
 
       <div className="w-full flex justify-center">
@@ -29,26 +55,33 @@ const Menu = () => {
             className="w-full px-4 py-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center xl:justify-items-start gap-x-4 gap-y-8"
           >
             {/* 
-              This will map all of items of FoodItems 
+              - This will map all of items of FoodItems 
               array variable inside of MenuItem component
-            */}
-            {FoodItems.map((foodItem: FoodItem, index: number) => (
-              <Sheet>
-                {/*
-                  This will trigger <MenuItemDetails /> component
-                  to slide in on right part of screen
-                */}
-                <SheetTrigger>
-                  <MenuItem key={index} foodItem={foodItem} />
-                </SheetTrigger>
 
-                {/* 
-                  A component containing all details of a 
-                  clicked item.
-                */}
-                <MenuItemDetails key={index} foodItem={foodItem} />
-              </Sheet>
-            ))}
+              - Conditional rendering based on filteredFoodItems
+            */}
+
+              {filteredFoodItems.length === 0 ? (
+                <p className="text-center text-gray-600">No Food Items Matched...</p>
+              ) : (     
+              filteredFoodItems.map((foodItem: FoodItem, index: number) => (
+                <Sheet>
+                  {/*
+                    This will trigger <MenuItemDetails /> component
+                    to slide in on right part of screen
+                  */}
+                  <SheetTrigger>
+                    <MenuItem key={index} foodItem={foodItem} />
+                  </SheetTrigger>
+
+                  {/* 
+                    A component containing all details of a 
+                    clicked item.
+                  */}
+                  <MenuItemDetails key={index} foodItem={foodItem} />
+                </Sheet>
+              ))
+            )}
           </div>
         </div>
       </div>
