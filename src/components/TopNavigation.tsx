34358d47e9176cbb199be6f6fb22fab74649a0f8
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-03-01 00:08:36 +0800

    IMPLEMENTED: MenuNavigation will only appear on TopNavigation component if the user is on route of '/menu'

--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -1,37 +1,22 @@
 import { useState, useEffect } from "react";
 import SideBar from "./SideBar";
 import { Button } from "./ui/button";
-import { Menu, Plus, XCircle } from "lucide-react";
-import { Input } from "./ui/input";
-import { Dialog, DialogTrigger } from "./ui/dialog";
-import { useDispatch, useSelector } from "react-redux";
-import { setSearchQuery } from "@/redux/features/search/searchSlice";
-import { RootState } from "@/redux/store";
-import { AddFoodForm } from "./modals/AddFood";
+import { Menu, XCircle } from "lucide-react";
+import { useLocation } from "react-router-dom";
+import MenuNavigation from "./MenuNavigation";
+
 
 const TopNavigation = () => {
-    const dispatch = useDispatch();
+    
+    const location = useLocation();
 
-    /*
-        This is handleSearchChange, a function the tracks the value being entered on Input component on search field.
+    // Check if the current route is the Menu route or component
+    const isMenuRoute = location.pathname === '/menu';
+    console.log(isMenuRoute);
 
-        The value being entered is being stored on setSearchQuery setter variable to be shared of all of components (ex. Menu)
-    */
-
-    const handleSearchChange = (event: any) => {
-        dispatch(setSearchQuery(event.target.value));
-    }
-    /*
-        This is searchQuery state variable that uses redux,
-
-        The value of searchQuery will be shared to 'Menu' component
-
-        So literally, what being entered on Input component on search field of this component is also being shared on Menu component.
-
-        'ON REAL TIME' 
-    */
-    const searchQuery = useSelector((state: RootState) => state.searcherQuery.searchQuery);
-
+    // // For debugging purposes
+    // const menuRoute = location.pathname;
+    // console.log(menuRoute);
 
     const [showSidebar, setShowSidebar] = useState(window.innerWidth >= 640);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -88,35 +73,7 @@ const TopNavigation = () => {
                     )
                 ) : null}
 
-                <div className="w-full flex justify-end sm:justify-center items-center">
-                    {/* Search and Add Button Wrapper */}
-                    <div className="flex md:gap-x-8 w-full sm:w-[80%] lg:w-[65%] xl:w-[55%] 2xl:w-[50%] justify-center  sm:justify-around md:justify-center lg:justify-around">
-                        {/* Search input */}
-                        <div className="flex w-[85%] max-w-[500px] 2xl:max-w-[650px] px-4">
-                            <Input 
-                                type="text"
-                                placeholder="Search..."
-                                value={searchQuery}
-                                onChange={handleSearchChange}
-                                className="text-gray-50 bg-sub-dark border-gray-700 shadow-md"
-                            />
-                        </div>
-
-                        <div className="flex sm:w-[5%] justify-center items-center">
-                            <Dialog>
-                            <DialogTrigger>
-                                <Button 
-                                    className="p-0"
-                                    variant="ghost"
-                                >
-                                    <Plus className="text-green-700" /> 
-                                </Button>
-                            </DialogTrigger>
-                            <AddFoodForm />
-                            </Dialog>
-                        </div>
-                    </div>
-                </div>
+                {isMenuRoute && <MenuNavigation />}
             </nav>
             {showSidebar && <SideBar />}
         </>
