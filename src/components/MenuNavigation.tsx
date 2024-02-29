b43eb9f61be6460a8ba01784a4fbb4e6e7097460
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-03-01 00:10:32 +0800

    CREATED: MenuNavigation component

---
+++ src/components/MenuNavigation.tsx
@@ -0,0 +1,53 @@
+// MenuNavigation.tsx
+
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchQuery } from "@/redux/features/search/searchSlice";
+import { RootState } from "@/redux/store";
+import { Input } from "./ui/input";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+import { Plus } from "lucide-react";
+import { AddFoodForm } from "./modals/AddFood";
+
+const MenuNavigation = () => {
+    const dispatch = useDispatch();
+    const searchQuery = useSelector((state: RootState) => state.searcherQuery.searchQuery);
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch(setSearchQuery(event.target.value));
+    };
+
+    return (
+        <div className="w-full flex justify-end sm:justify-center items-center">
+            <div className="flex md:gap-x-8 w-full sm:w-[80%] lg:w-[65%] xl:w-[55%] 2xl:w-[50%] justify-center  sm:justify-around md:justify-center lg:justify-around">
+                {/* Search input */}
+                <div className="flex w-[85%] max-w-[500px] 2xl:max-w-[650px] px-4">
+                    <Input 
+                        type="text"
+                        placeholder="Search..."
+                        value={searchQuery}
+                        onChange={handleSearchChange}
+                        className="text-gray-50 bg-sub-dark border-gray-700 shadow-md"
+                    />
+                </div>
+
+                <div className="flex sm:w-[5%] justify-center items-center">
+                    <Dialog>
+                        <DialogTrigger>
+                            <Button 
+                                className="p-0"
+                                variant="ghost"
+                            >
+                                <Plus className="text-green-700" /> 
+                            </Button>
+                        </DialogTrigger>
+                        <AddFoodForm />
+                    </Dialog>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MenuNavigation;
