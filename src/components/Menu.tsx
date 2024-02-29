844ad4e57e3de110514fd679039e38dc3d68078a
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-02-29 23:21:48 +0800

    IMPLEMENTED: searchQuery input data shared from TopNavigation component |--| ADDED: significant comments

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,23 +2,17 @@ import MenuItem from "./MenuItem"
 import MenuItemDetails from "./MenuItemDetails"
 
 import { Sheet, SheetTrigger } from "./ui/sheet"
-import { Dialog, DialogTrigger } from "./ui/dialog"
-import { Button } from "./ui/button"
-import { Input } from "./ui/input"
 
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "@/redux/store"
 
-import { setSearchQuery } from "@/redux/features/search/searchSlice"
 import { setFood } from "@/redux/features/food/foodSlice"
 
 import Category from "./Category"
-import { PlusCircle } from "lucide-react"
 
 import FirebaseConfig from "@/config/firebase";
 import { off, onValue, ref } from "firebase/database"
 
-import { AddFoodForm } from "./modals/AddFood"
 
 import { useEffect } from "react"
 import { FoodInterface } from "@/types"
@@ -27,11 +21,29 @@ const Menu = () => {
   const dispatch = useDispatch();
   const foods = useSelector((state: RootState) => state.fooder.foods)
 
+  /*
+    This is searchQuery state variable that uses redux,
+
+    The value of searchQuery will be coming from the 'TopNavigation' component
+
+    which also uses searchQuery on its input event value
+
+    So literally, what being entered on Input component of search field of TopNavigation component is also being shared on this component.
+
+    'ON REAL TIME' 
+  */
+  const searchQuery = useSelector((state: RootState) => state.searcherQuery.searchQuery)
+
+  // Filtered food items based on search query
+  const filteredFoods = foods ? foods.filter(food =>
+    food.name.toLowerCase().includes(searchQuery.toLowerCase()) 
+  ) : [];
+
+
   useEffect(() => {
     // Initialize Firebase
     const database = FirebaseConfig();
     const dbRef = ref(database, 'foodItems');
-
 
     // Retrieve data which is foods from Firebase
     onValue(dbRef, (snapshot) => {
@@ -40,13 +52,9 @@ const Menu = () => {
       if (data) {
         // Convert the object of food items to an array
         const foodsArray = Object.values(data);
-        console.log("Fetched foods from firebase:", foodsArray); // Log fetched data
         // Set 'foodsArray' on state'setFood' setter variable
         // So technically it will be stored on 'food' state variable
         dispatch(setFood(foodsArray))
-      } else {
-        console.log("No data fetched from firebase");
-        dispatch(setFood([])); // Ensure Redux store is updated with an empty array
       }
     })
 
@@ -57,17 +65,6 @@ const Menu = () => {
   }, []);
 
 
-  const searchQuery = useSelector((state: RootState) => state.searcherQuery.searchQuery)
-
-
-  const handleSearchChange = (event: any) => {
-    dispatch(setSearchQuery(event.target.value))
-  }
-
-  // Filtered food items based on search query
-  const filteredFoods = foods ? foods.filter(food =>
-    food.name.toLowerCase().includes(searchQuery.toLowerCase()) 
-  ) : [];
 
   return (
     <>
@@ -76,32 +73,6 @@ const Menu = () => {
           <h1 className="text-[2rem] sm:text-[3.5rem] md:text-[4rem] lg:text-[4.5rem] font-bold px-6">MENU MANAGER</h1>
         </div>
 
-      </div>
-
-      {/* Search input */}
-      <div className="w-full flex justify-center mt-4">
-        <div className="w-[350px] sm:w-[560px] md:w-[690px] lg:w-[940px] xl:w-[1200px] 2xl:w-[1350px] px-8 flex justify-between ">
-          <div className="flex w-[50%]">
-            <Input 
-              type="text"
-              placeholder="Search..."
-              value={searchQuery}
-              onChange={handleSearchChange}
-              className="text-gray-50 bg-sub-dark border-gray-700 shadow-md"
-            />
-          </div>
-
-          <div className="flex justify-center items-center">
-            <Dialog>
-              <DialogTrigger>
-                <Button className="bg-green-900">
-                  <PlusCircle /> <span>Add Food</span>
-                </Button>
-              </DialogTrigger>
-              <AddFoodForm />
-            </Dialog>
-          </div>
-        </div>
       </div>
 
       <div className="w-full min-h-10 flex justify-center items-center mt-1">
